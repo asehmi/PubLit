59946172037ff525c076f8ba510ba56e653569de
AvratanuBiswas <a.biswas@vu.nl>
2021-09-29 23:18:59 +0200

    about updated

--- publit.py
+++ publit.py
@@ -234,7 +234,7 @@ Thanks to the Streamlit Community for amazing [`Streamlit components`](https://d
 - [`Streamlit-Pyvis`](https://github.com/napoles-uach/stvis)
 - [`Streamlit-Lottie`](https://github.com/andfanilo/streamlit-lottie)
 
-### Further reads ...
+### Blogs to refer 
 - [`Network analysis to quickly get insight into an academic field with python`](https://towardsdatascience.com/network-analysis-to-quickly-get-insight-into-an-academic-field-with-python-cd891717d547)
 - [`Streamlit-AgGrid usage`](https://towardsdatascience.com/7-reasons-why-you-should-use-the-streamlit-aggrid-component-2d9a2b6e32f0)
 
